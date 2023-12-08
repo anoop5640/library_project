69d3b4180cdcd7ce1a522db9d944543784176e44
Anoop Kumar <anoop5640@gmail.com>
2023-12-08 22:06:26 +0000

    9

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,34 +1,20 @@
 #!/usr/bin/env bash
 
-# Navigate to the application directory
+# kill any servers that may be running in the background 
+sudo pkill -f runserver
+
+# kill frontend servers if you are deploying frontend
+# sudo pkill -f tailwind
+# sudo pkill -f node
+
 cd /home/ubuntu/library_project/
 
-# Activate virtual environment
+# activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
-# Install requirements
-pip install -r requirements.txt
+install requirements.txt
+pip install -r /home/ubuntu/library_project/requirements.txt
 
-# Start Gunicorn with 3 worker processes
-gunicorn --workers 3 library_project.wsgi:application --bind 0.0.0.0:8000 &
-
-# Nginx configuration
-cat <<EOF | sudo tee /etc/nginx/sites-available/library_project
-server {
-    listen 80;
-    server_name ec2-3-252-97-29.eu-west-1.compute.amazonaws.com;
-
-    location / {
-        proxy_pass http://127.0.0.1:8000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-EOF
-
-# Enable the site and restart Nginx
-sudo ln -sf /etc/nginx/sites-available/library_project /etc/nginx/sites-enabled
-sudo systemctl restart nginx
+# run server
+screen -d -m python3 manage.py runserver 0.0.0.0:8000
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
-# Clean codedeploy-agent files for a fresh install
+# clean codedeploy-agent files for a fresh install
 sudo rm -rf /home/ubuntu/install
 
-# Update and install necessary packages
+# install CodeDeploy agent
 sudo apt-get -y update
 sudo apt-get -y install ruby
 sudo apt-get -y install wget
-sudo apt-get -y install nginx
-
-# Install CodeDeploy agent
 cd /home/ubuntu
 wget https://aws-codedeploy-eu-west-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install 
 sudo ./install auto
 
-# Update OS and install Python 3 and its dependencies
+# update os & install python3
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
-# Delete existing application (if any)
+# delete app
 sudo rm -rf /home/ubuntu/library_project
