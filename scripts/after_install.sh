59c5315d4a1057bd87ff90f820ad8f490f4cacde
Anoop Kumar <anoop5640@gmail.com>
2023-12-09 00:18:35 +0000

    10

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,22 +1,44 @@
 #!/usr/bin/env bash
 
-# kill any servers that may be running in the background 
-sudo pkill -f runserver
+# Define your Django project name
+DJANGO_PROJECT_NAME='library_project'  # replace with your Django project name
 
-# kill frontend servers if you are deploying frontend
-# sudo pkill -f tailwind
-# sudo pkill -f node
+# Define the domain or public IP of your server
+SERVER_DOMAIN_OR_IP='3.252.97.29'  # replace with your domain or public IP
 
+# Navigate to the application directory
 cd /home/ubuntu/library_project/
 
-# activate virtual environment
-python3 -m venv venv
+# Activate the virtual environment
 source venv/bin/activate
 
-install requirements.txt
-pip install -r /home/ubuntu/library_project/requirements.txt
+# Install Python dependencies
+pip install -r requirements.txt
 
-# run server
-#python3 manage.py makemigrations
-#python3 manage.py migrate
-screen -d -m python3 manage.py runserver 0.0.0.0:8000
+# Start Gunicorn with 3 worker processes
+gunicorn --workers 3 $DJANGO_PROJECT_NAME.wsgi:application --bind 0.0.0.0:8000 &
+
+# Wait for a few seconds to ensure Gunicorn starts
+sleep 5
+
+# Create Nginx server block configuration
+sudo bash -c "cat > /etc/nginx/sites-available/library_project" << EOF
+server {
+    listen 80;
+    server_name $SERVER_DOMAIN_OR_IP;
+
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+EOF
+
+# Enable the Nginx server block by creating a symbolic link
+sudo ln -sf /etc/nginx/sites-available/library_project /etc/nginx/sites-enabled/library_project
+
+# Restart Nginx to apply the changes
+sudo systemctl restart nginx
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-# clean codedeploy-agent files for a fresh install
+# Clean codedeploy-agent files for a fresh install
 sudo rm -rf /home/ubuntu/install
 
-# install CodeDeploy agent
+# Update and install necessary packages
 sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
-cd /home/ubuntu
+sudo apt-get -y install ruby wget nginx
+
+# Install CodeDeploy agent
 wget https://aws-codedeploy-eu-west-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install 
 sudo ./install auto
 
-# update os & install python3
-sudo apt-get update
-sudo apt-get install -y python3 python3-dev python3-pip python3-venv
+# Update OS and install Python 3 and its dependencies
+sudo apt-get -y update
+sudo apt-get -y install python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
-# delete app
+# Delete existing application (if any)
 sudo rm -rf /home/ubuntu/library_project
